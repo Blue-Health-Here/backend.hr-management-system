931a832f20d3acf5766496c5cb075c0dcb1e198a
zaid-bluehealth <m_zaid@bluehealthhere.com>
2025-06-06 01:39:38 +0500

    shift basic controler route define

---
+++ src/controllers/working-days-controller.ts
@@ -0,0 +1,109 @@
+import { inject, injectable } from "tsyringe";
+import { FastifyReply, FastifyRequest, preHandlerHookHandler, RouteHandlerMethod } from "fastify";
+import { ControllerBase } from "./generics/controller-base";
+import { CommonRoutes } from "../constants/commonRoutes";
+import { IFetchRequest, IFilter, IGetSingleRecordFilter, IWorkingDaysRequest, IWorkingDaysResponse} from "../models";
+import { ExtendedRequest } from "../models/inerfaces/extended-Request";
+import { WorkingDaysService } from "../bl";
+import { authorize } from "../middlewares/authentication";
+import { payloadValidator, bodyValidator, queryValidator, paramsValidator } from "../middlewares/payload-validator";
+import { uuidParamSchema, createWorkingDaysSchema, updateWorkingDaysSchema} from "../models/payload-schemas";
+
+
+@injectable()
+export class WorkingDaysController extends ControllerBase {
+    constructor(@inject('WorkingDaysService') private readonly workingDaysService: WorkingDaysService){
+        super('/working-days');
+        this.middleware = authorize as preHandlerHookHandler;
+        this.endPoints = [
+            {
+                method: 'POST',
+                path: CommonRoutes.create,
+                middlewares: [bodyValidator(createWorkingDaysSchema)],
+                handler: this.add as RouteHandlerMethod
+            },
+            {
+                method: 'POST',
+                path: CommonRoutes.getAll,
+                handler: this.getAll as RouteHandlerMethod
+            },
+            {
+                method: 'GET',
+                path: `${CommonRoutes.getById}/:id`,
+                middlewares: [paramsValidator(uuidParamSchema)],
+                handler: this.getById as RouteHandlerMethod
+            },
+            {
+                method: 'POST',
+                path: CommonRoutes.getOneByQuery,
+                handler: this.getOneByQuery as RouteHandlerMethod
+            },
+            {
+                method: 'PUT',
+                path: `${CommonRoutes.update}/:id`,
+                middlewares: [
+                    paramsValidator(uuidParamSchema),
+                    bodyValidator(updateWorkingDaysSchema)
+                ],
+                handler: this.update as RouteHandlerMethod
+            },
+            {
+                method: 'DELETE',
+                path: `${CommonRoutes.delete}/:id`,
+                middlewares: [paramsValidator(uuidParamSchema)],
+                handler: this.delete as RouteHandlerMethod
+            }
+        ];
+
+    }
+
+
+    private add = async (req: FastifyRequest<{Body: IWorkingDaysRequest}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if(request.user){
+            res.send(await this.workingDaysService.add(req.body, request.user))
+        }
+    }
+
+    private getAll = async (req: FastifyRequest<{Body?: IFetchRequest<IWorkingDaysRequest>}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        res.send(await this.workingDaysService.get(request.user, req.body))
+        if(request.user){
+        }
+    }
+
+    private getById = async (req: FastifyRequest<{Params: {id: string}}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        res.send(await this.workingDaysService.getById(req.params.id, request.user));
+        if(request.user){
+        }
+    }
+
+    private getOneByQuery = async (req: FastifyRequest<{Body: IGetSingleRecordFilter<IWorkingDaysRequest>}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if (request.user) {
+          res.send(await this.workingDaysService.getOne(request.user, req.body));
+        }    
+    }
+ 
+    private delete = async (req: FastifyRequest<{Params: {id: string}}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if (request.user) {
+          res.send(await this.workingDaysService.delete(req.params.id, request.user));
+        }       
+    }
+
+    private update = async (req: FastifyRequest<{Body: IWorkingDaysRequest, Params: {id: string}}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if (request.user) {
+          res.send(await this.workingDaysService.update(req.params.id, req.body, request.user));
+        }  
+    }
+
+}
