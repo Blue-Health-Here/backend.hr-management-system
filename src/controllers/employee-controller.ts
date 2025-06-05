3be450025dae7353765f4671da025ea7b3337322
zaid-bluehealth <m_zaid@bluehealthhere.com>
2025-06-05 22:58:28 +0500

    Employee controller basic function handling

---
+++ src/controllers/employee-controller.ts
@@ -0,0 +1,109 @@
+import { inject, injectable } from "tsyringe";
+import { FastifyReply, FastifyRequest, preHandlerHookHandler, RouteHandlerMethod } from "fastify";
+import { ControllerBase } from "./generics/controller-base";
+import { CommonRoutes } from "../constants/commonRoutes";
+import { IFetchRequest, IFilter, IEmployeeRequest, IEmployeeResponse, IGetSingleRecordFilter} from "../models";
+import { ExtendedRequest } from "../models/inerfaces/extended-Request";
+import { EmployeeService } from "../bl";
+import { authorize } from "../middlewares/authentication";
+import { payloadValidator, bodyValidator, queryValidator, paramsValidator } from "../middlewares/payload-validator";
+import { uuidParamSchema, createEmployeeSchema, updateEmployeeSchema} from "../models/payload-schemas";
+
+
+@injectable()
+export class EmployeeController extends ControllerBase {
+    constructor(@inject('EmployeeService') private readonly employeeService: EmployeeService){
+        super('/employee');
+        this.middleware = authorize as preHandlerHookHandler;
+        this.endPoints = [
+            {
+                method: 'POST',
+                path: CommonRoutes.create,
+                middlewares: [bodyValidator(createEmployeeSchema)],
+                handler: this.add as RouteHandlerMethod
+            },
+            {
+                method: 'POST',
+                path: CommonRoutes.getAll,
+                handler: this.getAll as RouteHandlerMethod
+            },
+            {
+                method: 'GET',
+                path: `${CommonRoutes.getById}/:id`,
+                middlewares: [paramsValidator(uuidParamSchema)],
+                handler: this.getById as RouteHandlerMethod
+            },
+            {
+                method: 'POST',
+                path: CommonRoutes.getOneByQuery,
+                handler: this.getOneByQuery as RouteHandlerMethod
+            },
+            {
+                method: 'PUT',
+                path: `${CommonRoutes.update}/:id`,
+                middlewares: [
+                    paramsValidator(uuidParamSchema),
+                    bodyValidator(updateEmployeeSchema)
+                ],
+                handler: this.update as RouteHandlerMethod
+            },
+            {
+                method: 'DELETE',
+                path: `${CommonRoutes.delete}/:id`,
+                middlewares: [paramsValidator(uuidParamSchema)],
+                handler: this.delete as RouteHandlerMethod
+            }
+        ];
+
+    }
+
+
+    private add = async (req: FastifyRequest<{Body: IEmployeeRequest}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if(request.user){
+            res.send(await this.employeeService.add(req.body, request.user))
+        }
+    }
+
+    private getAll = async (req: FastifyRequest<{Body?: IFetchRequest<IEmployeeRequest>}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        res.send(await this.employeeService.get(request.user, req.body))
+        if(request.user){
+        }
+    }
+
+    private getById = async (req: FastifyRequest<{Params: {id: string}}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        res.send(await this.employeeService.getById(req.params.id, request.user));
+        if(request.user){
+        }
+    }
+
+    private getOneByQuery = async (req: FastifyRequest<{Body: IGetSingleRecordFilter<IEmployeeRequest>}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if (request.user) {
+          res.send(await this.employeeService.getOne(request.user, req.body));
+        }    
+    }
+ 
+    private delete = async (req: FastifyRequest<{Params: {id: string}}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if (request.user) {
+          res.send(await this.employeeService.delete(req.params.id, request.user));
+        }       
+    }
+
+    private update = async (req: FastifyRequest<{Body: IEmployeeRequest, Params: {id: string}}>, res: FastifyReply) => {
+        let request = req as ExtendedRequest;
+
+        if (request.user) {
+          res.send(await this.employeeService.update(req.params.id, req.body, request.user));
+        }  
+    }
+
+}
